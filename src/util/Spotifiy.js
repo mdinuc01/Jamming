c9a658c890d8fb3be80b655463f433e8d3cc417e
Michael Dinuccio <mdinucci@my.centennialcollege.ca>
2023-02-07 13:16:16 -0500

    created Spotify module with functionality to obtain a Spotify access token

---
+++ src/util/Spotifiy.js
@@ -0,0 +1,26 @@
+let accessToken;
+const clientId = process.env.CLIENT_ID;
+const redirectUri = "http://localhost:3000/";
+
+const Spotify = {
+  getAccessToken() {
+    if (accessToken) return accessToken;
+
+    //check for access token match
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+      window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
+      window.history.pushState("Access Token", null, "/");
+      return accessToken;
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+      window.location = accessUrl;
+    }
+  }
+};
+
+export default Spotify;
